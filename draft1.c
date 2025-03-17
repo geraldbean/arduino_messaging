497ad73213b0d0791587ba34f89a1f99e919f6a1
Eliana-JH <22rcd3@queensu.ca>
2025-03-17 14:42:16 -0400

    Got Alphabet to Work

--- draft1.c
+++ draft1.c
@@ -2,13 +2,13 @@
 //Mar 4 6:51 PM
 
 // LCD pin connections: RS, E, D4, D5, D6, D7
-const int pin_RS = 8; 
-const int pin_EN = 9; 
-const int pin_d4 = 4; 
-const int pin_d5 = 5; 
-const int pin_d6 = 6; 
-const int pin_d7 = 7; 
-const int pin_BL = 10; 
+const int pin_RS = 8;
+const int pin_EN = 9;
+const int pin_d4 = 4;
+const int pin_d5 = 5;
+const int pin_d6 = 6;
+const int pin_d7 = 7;
+const int pin_BL = 10;
 LiquidCrystal lcd(pin_RS,  pin_EN,  pin_d4,  pin_d5,  pin_d6,  pin_d7);
 //LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
@@ -20,13 +20,15 @@ LiquidCrystal lcd(pin_RS,  pin_EN,  pin_d4,  pin_d5,  pin_d6,  pin_d7);
 #define SELECT 6
 
 // Variables
-char letters[]={'A','B','C','D','E','F','G','H','I','J','K','L','M','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+char letters[]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 //char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // Available letters
-int letterIndex = 0;  // Current letter
+int rowIndex = 0;  // Current letter
+int tempLetterIndex=rowIndex;
 int cursorPos = 0;    // Cursor column position on LCD
+int tempCursorPos=cursorPos;
 char typedText[17]; // Stores typed text
 bool flagPressed;//check if buttons were pressed or not
-char boardAlphabet[2][16];
+char boardAlphabet[2][13];
 char boardPrint[16];
 
 
@@ -35,15 +37,26 @@ void setup() {
   lcd.begin(16, 2); // Set LCD size
   lcd.setCursor(0,0);
   lcd.print("Select Letter:");
-  lcd.setCursor(0,1);
-  lcd.print(letters[0]);
   int letterIndexSetup=0;
   //setting up char array for board and its print array
-  for (int i=0;i<16;i++){
+  //Serial.println("boardAlphabet array:");//print statement check
+  for (int i=0;i<13;i++){
     boardAlphabet[0][i]=letters[letterIndexSetup++];//puts it to letter index, then adds one to it for next letter
     boardAlphabet[1][i]=letters[letterIndexSetup++];
     boardPrint[i]=boardAlphabet[0][i];
+    // Serial.print(boardAlphabet[0][i]);
+    // Serial.print("  ");
+    // Serial.println(boardAlphabet[1][i]);//print statement checks
   }
+  for (int i=13;i<16;i++){
+    boardPrint[i]= NULL;
+  }
+  //test print
+  // //Serial.println("boardPrint array:");//print statement checks
+  // for (int i=0;i<16;i++){
+  //   Serial.print(boardPrint[i]);
+  //   Serial.print("  ");
+  // }
   letterIndexSetup=0;
   for (int i=0;i<16;i++){
     lcd.setCursor(i,1);//set to second row's columns
@@ -59,36 +72,63 @@ void setup() {
 void loop() {
   int x=analogRead(0);
   if (x>800) flagPressed=0;
-  //conditional of button press type and "&&" is to check if the flag was previously not being pressed 
+  //conditional of button press type and "&&" is to check if the flag was previously not being pressed
   //(thus ensuring any duration of button press will work)
-  
+ 
   //if Right pressed
   if (x <6 && flagPressed==0) {
     flagPressed=1;
-    cursorPos = min(15, cursorPos + 1); // Move cursor right
+    tempCursorPos=cursorPos;
+    cursorPos = min(15, tempCursorPos + 1); // Move cursor right
     lcd.setCursor(cursorPos,1);
+    //Serial.print("new cursor position:"); //print statement checks
+    Serial.println(cursorPos);
   }
   //if Up pressed
   else if (x<200 && flagPressed==0) {
+    if(cursorPos>12){
+      //up button for our functions
+    }else{
     flagPressed=1;
-    letterIndex = (letterIndex + 1) % strlen(letters); // Next letter
+    // Serial.print("current cursors position:");
+    // Serial.println(cursorPos);
+    // Serial.print("current boardPrint[cursorPos] value");
+    // Serial.println(boardPrint[cursorPos]);//print statement checks
+    if(rowIndex==0){
+      rowIndex=1;
+    }else{
+      rowIndex=0;
+    }
+    boardPrint[cursorPos] = boardAlphabet[rowIndex][cursorPos];
+    lcd.print(boardPrint[cursorPos]);}
     delay(200);
   }
   //if Down pressed
   else if (x<400 && flagPressed==0) {
-    flagPressed=1;
-    letterIndex = (letterIndex - 1 + strlen(letters)) % strlen(letters); // Previous letter
+    flagPressed=1;  
+    if(cursorPos>12){
+      //up button for our functions
+    }else{if(rowIndex==0){
+      rowIndex=1;
+    }else{
+      rowIndex=0;
+    }
+    boardPrint[cursorPos] = boardAlphabet[rowIndex][cursorPos];
+    lcd.print(boardPrint[cursorPos]);}
     delay(200);
   }
-  
+  //if Left pressed
   else if (x<600 && flagPressed==0) {
     flagPressed=1;
-    cursorPos = max(0, cursorPos - 1); // Move cursor left
+    tempCursorPos=cursorPos;
+    cursorPos = max(0, tempCursorPos - 1); // Move cursor left
     lcd.setCursor(cursorPos,1);
+    //Serial.print("new cursor position:");//check print statement
+    Serial.println(cursorPos);
   }
   //if Select pressed
   else if (x<800 && flagPressed==0) {
-    typedText[cursorPos] = letters[letterIndex]; // Select letter
+    typedText[cursorPos] = letters[rowIndex]; // Select letter
     delay(200);
   }
 
