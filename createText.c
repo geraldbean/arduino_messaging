1b1a9fa582842ff73052a73f0330fe0cfb81cd39
Eliana-JH <22rcd3@queensu.ca>
2025-03-27 15:52:21 -0400

    SetupInLoop&Select
    setup in loop, select code written

--- createText.c
+++ createText.c
@@ -18,6 +18,7 @@ LiquidCrystal lcd(pin_RS,  pin_EN,  pin_d4,  pin_d5,  pin_d6,  pin_d7);
 #define UP 4
 #define DOWN 5
 #define SELECT 6
+#define MAX_TEXT 5
 
 // Variables
 char letters[]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
@@ -26,7 +27,9 @@ int rowIndex = 0;  // Current letter
 int tempLetterIndex=rowIndex;
 int cursorPos = 0;    // Cursor column position on LCD
 int tempCursorPos=cursorPos;
+char selectedText[MAX_TEXT];
 char typedText[17]; // Stores typed text
+int textEmptyIndex=0;
 bool flagPressed;//check if buttons were pressed or not
 char boardAlphabet[2][13];
 char boardPrint[16];
@@ -34,39 +37,7 @@ char boardPrint[16];
 
 void setup() {
   Serial.begin(9600);
-  lcd.begin(16, 2); // Set LCD size
-  lcd.setCursor(0,0);
-  lcd.print("Select Letter:");
-  int letterIndexSetup=0;
-  //setting up char array for board and its print array
-  //Serial.println("boardAlphabet array:");//print statement check
-  for (int i=0;i<13;i++){
-    boardAlphabet[0][i]=letters[letterIndexSetup++];//puts it to letter index, then adds one to it for next letter
-    boardAlphabet[1][i]=letters[letterIndexSetup++];
-    boardPrint[i]=boardAlphabet[0][i];
-    // Serial.print(boardAlphabet[0][i]);
-    // Serial.print("  ");
-    // Serial.println(boardAlphabet[1][i]);//print statement checks
-  }
-  for (int i=13;i<16;i++){
-    boardPrint[i]= NULL;
-  }
-  //test print
-  // //Serial.println("boardPrint array:");//print statement checks
-  // for (int i=0;i<16;i++){
-  //   Serial.print(boardPrint[i]);
-  //   Serial.print("  ");
-  // }
-  letterIndexSetup=0;
-  for (int i=0;i<16;i++){
-    lcd.setCursor(i,1);//set to second row's columns
-    if (boardPrint[i]){
-     lcd.print(boardPrint[i]);
-    }
-    else lcd.print(" "); //ensures if no more letters doesn't print hieroglyphs
- }
-  lcd.setCursor(0,1);
-  // // updateDisplay();
+  textSetup(letters,boardAlphabet,boardPrint);
 }
 
 void loop() {
@@ -82,7 +53,7 @@ void loop() {
     cursorPos = min(15, tempCursorPos + 1); // Move cursor right
     lcd.setCursor(cursorPos,1);
     //Serial.print("new cursor position:"); //print statement checks
-    Serial.println(cursorPos);
+    //Serial.println(cursorPos);
   }
   //if Up pressed
   else if (x<200 && flagPressed==0) {
@@ -124,15 +95,69 @@ void loop() {
     cursorPos = max(0, tempCursorPos - 1); // Move cursor left
     lcd.setCursor(cursorPos,1);
     //Serial.print("new cursor position:");//check print statement
-    Serial.println(cursorPos);
+    //Serial.println(cursorPos);
   }
   //if Select pressed
   else if (x<800 && flagPressed==0) {
-    typedText[cursorPos] = letters[rowIndex]; // Select letter
+    if (cursorPos==15 && textEmptyIndex!=0){
+      textEmptyIndex--;
+      Serial.print("Deleted from msg:");
+      Serial.println(selectedText[textEmptyIndex]);
+      selectedText[textEmptyIndex]=NULL;
+    }
+    if (textEmptyIndex<MAX_TEXT){
+      if (cursorPos<13){
+        selectedText[textEmptyIndex] = boardPrint[cursorPos]; // Select letter
+        Serial.print(selectedText[textEmptyIndex]);
+        textEmptyIndex++;//increment to indicate next empty index
+      }
+    }else {
+      Serial.print("Message Max Reached:");
+      for(int i=0; i<MAX_TEXT;i++){
+        Serial.print(selectedText[i]);
+      }
+      Serial.println("");
+    }
     delay(200);
   }
 
   updateDisplay();
+}
+
+void textSetup(char* letters, char boardAlphabet[2][13], char* boardPrint){
+  lcd.begin(16, 2); // Set LCD size
+  lcd.setCursor(0,0);
+  lcd.print("Select Letter:");
+  int letterIndexSetup=0;
+  //setting up char array for board and its print array
+  //Serial.println("boardAlphabet array:");//print statement check
+  for (int i=0;i<13;i++){
+    boardAlphabet[0][i]=letters[letterIndexSetup++];//puts it to letter index, then adds one to it for next letter
+    boardAlphabet[1][i]=letters[letterIndexSetup++];
+    boardPrint[i]=boardAlphabet[0][i];
+    // Serial.print(boardAlphabet[0][i]);
+    // Serial.print("  ");
+    // Serial.println(boardAlphabet[1][i]);//print statement checks
+  }
+  for (int i=13;i<16;i++){
+    boardPrint[i]= NULL;
+  }
+  //test print
+  // //Serial.println("boardPrint array:");//print statement checks
+  // for (int i=0;i<16;i++){
+  //   Serial.print(boardPrint[i]);
+  //   Serial.print("  ");
+  // }
+  letterIndexSetup=0;
+  for (int i=0;i<16;i++){
+    lcd.setCursor(i,1);//set to second row's columns
+    if (boardPrint[i]){
+     lcd.print(boardPrint[i]);
+    }
+    else lcd.print(" "); //ensures if no more letters doesn't print hieroglyphs
+ }
+  lcd.setCursor(0,1);
+  // // updateDisplay();
 }
 
 void updateDisplay() {
